bd500cd00f2e880ff5e0a257aa4e173b3e4a0633
ninetailsrabbit <danieldigitalspace@proton.me>
2025-03-01 11:56:11 +0000

    added simple draggable 2d component

--- addons/ninetailsrabbit.indie_blueprint_toolbox/ninetailsrabbit.indie_blueprint_toolbox.gd
+++ addons/ninetailsrabbit.indie_blueprint_toolbox/ninetailsrabbit.indie_blueprint_toolbox.gd
@@ -4,14 +4,23 @@ extends EditorPlugin
 
 func _enter_tree() -> void:
 	add_autoload_singleton("IndieBlueprintWindowManager", "res://addons/ninetailsrabbit.indie_blueprint_toolbox/src/autoloads/viewport/window_manager.gd")
+	
 	add_custom_type(
 		"IndieBlueprintSmartDecal", 
 		"Decal", 
 		preload("res://addons/ninetailsrabbit.indie_blueprint_toolbox/src/components/3D/decals/smart_decal.gd"),
 		null
 	)
+	
+	add_custom_type(
+		"IndieBlueprintDraggable2D", 
+		"Node2D", 
+		preload("res://addons/ninetailsrabbit.indie_blueprint_toolbox/src/components/2D/drag/draggable_2d.gd"),
+		preload("res://addons/ninetailsrabbit.indie_blueprint_toolbox/src/components/2D/drag/draggable_2d.svg")
+	)
 
 
 func _exit_tree() -> void:
+	remove_custom_type("IndieBlueprintDraggable2D")
 	remove_custom_type("SmartDecal")
 	remove_autoload_singleton("IndieBlueprintWindowManager")
