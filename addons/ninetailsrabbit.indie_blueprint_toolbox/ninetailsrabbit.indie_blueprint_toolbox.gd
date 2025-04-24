7abe04caafe57e7bdcc517c29766009359bf1bfd
ninetailsrabbit <danieldigitalspace@proton.me>
2025-04-24 10:07:57 +0100

    new token methods on stringh elper

--- addons/ninetailsrabbit.indie_blueprint_toolbox/ninetailsrabbit.indie_blueprint_toolbox.gd
+++ addons/ninetailsrabbit.indie_blueprint_toolbox/ninetailsrabbit.indie_blueprint_toolbox.gd
@@ -233,7 +233,7 @@ func _create_preload_section(preloader: FileAccess, section: String, file_paths:
 				"wav":
 					type = ": AudioStreamWAV"
 				
-		var constant_name: String =  path.get_file().trim_suffix("." + extension).replace(".", "_").to_pascal_case() + suffix
+		var constant_name: String =  path.get_file().trim_suffix("." + extension).replace(".-", "_").to_pascal_case() + suffix
 		
 		if processed_names.has(constant_name) or ClassDB.class_exists(constant_name):
 			var frequency: Dictionary = IndieBlueprintArrayHelper.frequency([constant_name])
