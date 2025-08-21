4a2eb3a4dee953036702bc84971d8ee1cc92dc05
sempitern0 <danieldigitalspace@proton.me>
2025-08-21 16:38:51 +0100

    local network handler can now create broadcast listeners to decode received packets

--- addons/ninetailsrabbit.indie_blueprint_toolbox/src/autoloads/network/local_network_handler.gd
+++ addons/ninetailsrabbit.indie_blueprint_toolbox/src/autoloads/network/local_network_handler.gd
@@ -14,6 +14,7 @@ const DefaultBroadcastPort: int = 42070
 const DefaultBroadcastListenPort: int = 42071
 
 var broadcaster: PacketPeerUDP
+var broadcast_listener: PacketPeerUDP
 var broadcast_timer: Timer
 var broadcast_emission_interval: int = 1
 var current_broadcast_emission: PackedByteArray
@@ -54,31 +55,39 @@ func start_client(ip: String = IpAddress, port: int = DefaultServerPort) -> void
 	multiplayer.server_disconnected.connect(on_server_disconnected)
 
 
-func start_broadcast() -> void:
+func start_broadcast(broadcast_port: int = DefaultBroadcastPort, dest_port: int = DefaultBroadcastListenPort, bind_address: String = "0.0.0.0") -> void:
 	_create_broadcast_timer()
 		
 	broadcaster = PacketPeerUDP.new()
 	broadcaster.set_broadcast_enabled(true)
-	broadcaster.set_dest_address(BroadcastAddress, DefaultBroadcastListenPort)
-	var binded_port_error: Error =  broadcaster.bind(DefaultBroadcastPort, "0.0.0.0")
+	broadcaster.set_dest_address(BroadcastAddress, dest_port)
+	var binded_port_error: Error =  broadcaster.bind(broadcast_port, bind_address)
 	
 	if binded_port_error == OK:
-		print("LocalNetworkHandler: Broadcast port %d binded with success " % DefaultBroadcastPort)
+		print("LocalNetworkHandler: Broadcast port %d binded with success " % broadcast_port)
+	else:
+		push_error("LocalNetworkHandler: An error %s happened when binding port on broadcast %d" % [error_string(binded_port_error), broadcast_port])
 		
 	broadcast_timer.start(broadcast_emission_interval)
+
+## To decode packets received you can do:
+## 	if broadcast_listener and broadcast_listener.get_available_packet_count() > 0:
+##		var server_bytes_data: Dictionary = JSON.parse_string(broadcast_listener.get_packet().get_string_from_ascii())
+
+func start_broadcast_listener(listen_port: int = DefaultBroadcastListenPort, bind_address: String = "0.0.0.0") -> PacketPeerUDP:
+	if broadcast_listener:
+		broadcast_listener.close()
+	else:
+		broadcast_listener = PacketPeerUDP.new()
 		
-
-func end() -> void:
-	end_broadcast()
-	multiplayer.multiplayer_peer = null
-
-
-func end_broadcast() -> void:
-	if is_instance_valid(broadcast_timer):
-		broadcast_timer.stop()
+	var binded_port_error: Error =  broadcast_listener.bind(listen_port, bind_address)
 	
-	if broadcaster:
-		broadcaster.close()
+	if binded_port_error == OK:
+		print("LocalNetworkHandler: Listener broadcast port %d binded with success " % listen_port)
+	else:
+		push_error("LocalNetworkHandler: An error %s happened when binding port on broadcast listener %d" % [error_string(binded_port_error), listen_port])
+		
+	return broadcast_listener
 
 
 func get_local_ip() -> String:
@@ -109,6 +118,30 @@ func get_broadcast_address(local_ip: String) -> String:
 	else:
 		return "127.0.0.1" if use_localhost else "255.255.255.255"
 	
+	
+func set_current_broadcast_emission(packet: PackedByteArray) -> void:
+	if packet.size() > 0:
+		current_broadcast_emission = packet
+
+
+func end() -> void:
+	end_broadcast()
+	end_broadcast_listener()
+	multiplayer.multiplayer_peer = null
+
+
+func end_broadcast() -> void:
+	if is_instance_valid(broadcast_timer):
+		broadcast_timer.stop()
+	
+	if broadcaster:
+		broadcaster.close()
+
+
+func end_broadcast_listener() -> void:
+	if broadcast_listener:
+		broadcast_listener.close()
+	
 
 func _create_broadcast_timer() -> void:
 	if not is_instance_valid(broadcast_timer):
@@ -120,10 +153,6 @@ func _create_broadcast_timer() -> void:
 		add_child(broadcast_timer)
 		broadcast_timer.timeout.connect(on_broadcast_timer_timeout)
 
-
-func set_current_broadcast_emission(packet: PackedByteArray) -> void:
-	if packet.size() > 0:
-		current_broadcast_emission = packet
 
 #region Signal callbacks
 func on_broadcast_timer_timeout() -> void:
